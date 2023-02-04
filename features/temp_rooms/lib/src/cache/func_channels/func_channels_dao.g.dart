f230d831c93cb14aa2017acd22a3459725e309cd
Des1ynY <i.smetanin.dev@gmail.com>
2023-02-04 19:03:40 +0500

    generated files removed from gitignore

---
+++ features/temp_rooms/lib/src/cache/func_channels/func_channels_dao.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'func_channels_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$FuncChannelsDaoMixin on DatabaseAccessor<Cache> {
+  $FuncChannelsTableTable get funcChannelsTable =>
+      attachedDatabase.funcChannelsTable;
+}
